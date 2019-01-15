a97e5cfb28c5f93d9a9a22b24c5769544cf9254d
Martin André <m.andre@redhat.com>
2019-01-15 11:25:23 +0100

    Remove obsolete workaround

--- undercloud_scripts/01_install_undercloud.sh
+++ undercloud_scripts/01_install_undercloud.sh
@@ -5,9 +5,3 @@ source $SCRIPTDIR/common.sh
 
 sudo setenforce 0
 sudo sed -i "s/^SELINUX=enforcing/SELINUX=permissive/" /etc/selinux/config
-
-# Workaround issue with repos in overcloud-full image
-sudo yum install -y libguestfs-tools
-virt-customize --selinux-relabel -a $HOME/overcloud-full.qcow2 --install yum-plugin-priorities
-virt-customize --selinux-relabel -a $HOME/overcloud-full.qcow2 --upload $SCRIPTDIR/quickstart-centos-virt-container.repo:/etc/yum.repos.d/
-openstack overcloud image upload --update-existing
