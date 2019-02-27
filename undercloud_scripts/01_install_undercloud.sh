db08812248f2748cdcf773ed09032df7cbbda5d7
Martin André <m.andre@redhat.com>
2019-02-27 15:35:20 +0100

    Stop explicitely disabling selinux
    Quickstart does it for us

--- undercloud_scripts/01_install_undercloud.sh
+++ undercloud_scripts/01_install_undercloud.sh
@@ -3,5 +3,3 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $SCRIPTDIR/common.sh
 
-sudo setenforce 0
-sudo sed -i "s/^SELINUX=enforcing/SELINUX=permissive/" /etc/selinux/config
