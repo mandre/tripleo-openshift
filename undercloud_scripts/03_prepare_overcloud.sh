98e5b631fdd924fa09984d780a6bd69c629d6311
Martin André <m.andre@redhat.com>
2019-02-27 15:37:55 +0100

    No need to deploy overcloud

--- undercloud_scripts/03_prepare_overcloud.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $SCRIPTDIR/common.sh
-
-echo -e "parameter_defaults:\n  CAMap:"|tee /home/stack/inject-trust-anchor-hiera.yaml
-UC_SSL_CERT=`sed ':a;N;$!ba;s/\n/\n        /g' /etc/pki/ca-trust/source/anchors/cm-local-ca.pem`
-echo -n -e "    undercloud-ca:\n      content: |\n        "|tee -a /home/stack/inject-trust-anchor-hiera.yaml
-echo "$UC_SSL_CERT"|tee -a /home/stack/inject-trust-anchor-hiera.yaml
