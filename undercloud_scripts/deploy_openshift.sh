25ae6605c9cac7082b968b2f99901a96885c6ba7
Martin André <m.andre@redhat.com>
2019-03-04 14:00:52 +0100

    Rename 06_deploy_openshift.sh to deploy_openshift.sh
    Make it understand the --redeploy option to allow cleaning stack from
    previous deployment.
    
    Also detect existing stack and warn about it when deploying over an
    already deployed openshift.

---
+++ undercloud_scripts/deploy_openshift.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+SCRIPTDIR=$(python -c "import os;print os.path.dirname(os.path.realpath('$0'))")
+source $SCRIPTDIR/common.sh
+
+usage () {
+  echo "Usage: $(basename $0) [options]"
+    echo ""
+    echo "Basic options:"
+    echo "  --redeploy          clean-up existing stack before deploying again"
+    echo "  -h, --help          print this help and exit"
+}
+
+REMOVE_OLD_STACK=${REMOVE_OLD_STACK:-}
+
+OPTS=`getopt -o h --long help,redeploy -- "$@"`
+eval set -- "$OPTS"
+
+while true; do
+  case "$1" in
+    -h | --help)
+      usage; exit ;;
+    --redeploy)
+      export REMOVE_OLD_STACK=1; shift ;;
+    --) shift ; break ;;
+    * ) break ;;
+  esac
+done
+
+if [[ $REMOVE_OLD_STACK ]]; then
+  echo Removing previous deployment stack...
+  openstack stack delete -y --wait $OPENSHIFT_STACK_NAME || true
+elif openstack stack list | grep -q $OPENSHIFT_STACK_NAME; then
+  echo WARNING openshift is already deployed, assuming udate
+  echo Press Ctl-C to cancel
+  sleep 10
+fi
+
+if [[ $OPENSHIFT_CNS ]]; then
+  CNS_ENV="-e $HOME/tripleo-heat-templates/environments/openshift-cns.yaml"
+fi
+
+# Deploy the openshift stack
+pushd $HOME
+openstack overcloud deploy \
+  --templates $HOME/tripleo-heat-templates \
+  --disable-validations \
+  --stack ${OPENSHIFT_STACK_NAME} \
+  -r $HOME/openshift_roles_data.yaml \
+  -n $HOME/tripleo-heat-templates/network_data_openshift.yaml \
+  -e $HOME/tripleo-heat-templates/environments/openshift.yaml \
+  ${CNS_ENV} \
+  -e $HOME/tripleo-heat-templates/environments/network-isolation.yaml \
+  -e $HOME/tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml \
+  -e $HOME/openshift_env.yaml \
+  -e $HOME/openshift_docker_images.yaml \
+  ${OPENSHIFT_STACK_EXTRA_ARGS}
+
+popd
